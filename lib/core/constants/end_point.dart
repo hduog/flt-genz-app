b7f3f126131c52cf7b0a09d13a2cc3b7cd83ede2
duong.nh <d.xuan@humain.co.jp>
2024-11-30 22:28:30 +0700

    fix bug

--- lib/core/constants/end_point.dart
+++ lib/core/constants/end_point.dart
@@ -1,6 +1,6 @@
 class ApiEndPointConstants {
   const ApiEndPointConstants._();
-  static const String _baseUrl = "http://192.168.0.195:3003/api";
+  static const String _baseUrl = "http://10.0.2.2:3003/api";
 
   static String apiLogin = "$_baseUrl/auth/login";
   static String apiRegistration = "$_baseUrl/auth/register";
--- lib/ui/widget/blogs.card.dart
+++ lib/ui/widget/blogs.card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/constants/constants.dart';
 import 'package:flutter_application_1/core/data/models/BlogModel/BlogData/BlogData.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:html_unescape/html_unescape.dart';
 
@@ -12,75 +11,78 @@ class BlogsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final unescape = HtmlUnescape();
-final bodyText = unescape.convert(blogItem.body ?? "").replaceAll(RegExp(r'<[^>]*>'), '');
+    final bodyText = unescape
+        .convert(blogItem.body ?? "")
+        .replaceAll(RegExp(r'<[^>]*>'), '');
 
-    return Container(
-      margin: EdgeInsets.only(top: 15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Image.network(
-                '${Constants.awsUrl}${blogItem.thumbnailBlog}',
-                fit: BoxFit.cover,
-                width: 50,
-                height: 50,
-              ),
-            
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              '${Constants.awsUrl}${blogItem.thumbnailBlog}',
+              fit: BoxFit.cover,
+              width: 130,
+            ),
           ),
-          SizedBox(width: 10,),
-          Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                blogItem.title ?? "",
-                softWrap: true,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                    color: colorTextCateBlog, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width *
-                    0.9, // Chiều rộng là 90% màn hình
-                child: Text(
-                  // blogItem.body ?? "",
-                  bodyText,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: SizedBox(
+            height: 120,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  blogItem.title ?? "",
                   softWrap: true,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 3,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
+                  style: const TextStyle(
+                      color: colorTextCateBlog, fontWeight: FontWeight.w500),
                 ),
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Text(
-                    formatDate(blogItem.created_at) ?? "DD/MM/YYYY HH:mm",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: colorTextSubPart,
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Text(
+                    bodyText,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  SizedBox(width: 20),
-                  Text("${blogItem.views ?? 0} lượt xem",
-                      style: TextStyle(fontSize: 12, color: colorTextSubPart))
-                ],
-              )
-            ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      formatDate(blogItem.created_at) ?? "DD/MM/YYYY HH:mm",
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: colorTextSubPart,
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Text("${blogItem.views ?? 0} lượt xem",
+                        style: const TextStyle(
+                            fontSize: 12, color: colorTextSubPart))
+                  ],
+                )
+              ],
+            ),
           ),
-          ),
-          SvgPicture.asset(
-            'assets/icons/dots-vertical.svg',
-            width: 20,
-            height: 20,
-          ),
-        ],
-      ),
+        ),
+        SvgPicture.asset(
+          'assets/icons/dots-vertical.svg',
+          width: 20,
+          height: 20,
+        ),
+      ],
     );
   }
 }
